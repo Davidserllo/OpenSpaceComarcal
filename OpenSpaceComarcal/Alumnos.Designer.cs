224faae279cc9e815a28915b81264cf1ee8ee79c
Davidserllo <davserllo@gmail.com>
2024-03-27 11:50:34 +0100

    boton

--- OpenSpaceComarcal/Alumnos.Designer.cs
+++ OpenSpaceComarcal/Alumnos.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(500, 147);
+            this.button1.Location = new System.Drawing.Point(647, 371);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
