3e3a5af02ee00d32a54b8b4c895fed276c32857f
Davidserllo <davserllo@gmail.com>
2024-05-07 10:22:21 +0200

    Cambios funcionamiento de cambio de aparencia 🌼

--- OpenSpaceComarcal/Libraries/Apariencia.cs
+++ OpenSpaceComarcal/Libraries/Apariencia.cs
@@ -1,21 +1,38 @@
 ﻿using Sunisoft.IrisSkin;
 using System;
+using System.IO;
 
 namespace OpenSpaceComarcal.Libraries
 {
     internal class Apariencia
     {
+        // Ruta relativa de las apariencias
+        public static string RelativePath = @"..\..\Resources\TemasInterfaz\Emerald\";
         public static void AplicarApariencia(SkinEngine skinEngine)
         {
+            // Obtener nombre de la apariencia a aplicar
             String NombreApariencia = Properties.Settings.Default.NomApariencia;
-            skinEngine.SkinFile = NombreApariencia;
+
+            // Localizar ruta de la apariencia
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = Path.GetFullPath(Path.Combine(basePath, RelativePath));
+
+            // Aplicar la apariencia
+            skinEngine.SkinFile = fullPath + NombreApariencia;
         }
 
         public static void CambiarApariencia(SkinEngine skinEngine, String NombreApariencia)
         {
+            // Cambiar aparencia y guardar los cambios
             Properties.Settings.Default.NomApariencia = NombreApariencia;
             Properties.Settings.Default.Save();
-            skinEngine.SkinFile = NombreApariencia;
+
+            // Localizar ruta de la apariencia
+            string basePath = AppDomain.CurrentDomain.BaseDirectory;
+            string fullPath = Path.GetFullPath(Path.Combine(basePath, RelativePath));
+
+            // Aplicar la apariencia
+            skinEngine.SkinFile = fullPath + NombreApariencia;
         }
 
     }
